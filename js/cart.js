8f93367ff245c8b02ed2c4e658d7941636d0cae5
mohfadel2022 <mohfadel2016@gmail.com>
2024-06-02 02:26:45 +0200

    Generate HTML for order summary

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,30 @@
-export const cart = []
+export const cart = [
+    {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 3
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 2
+    }
+]
 
 export function addToCart(productId){
-    let matchedItem = ''
+    let matchedItem
         
-        cart.forEach((cartItem) => {
-            if (productId === cartItem.productId){
-                matchedItem = catItem
-            }
-        })
-        if (matchedItem){
-            matchedItem.quantity += 1
-        } else{
-            cart.push({
-                productId: productId,
-                quantity: 1
-            })        
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId){
+            matchedItem = catItem
         }
+    })
+    if (matchedItem){
+        matchedItem.quantity += 1
+    } else{
+        cart.push({
+            productId: productId,
+            quantity: 1
+        })        
+    }
 
 }
 
